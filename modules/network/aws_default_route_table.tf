d21bdce0b8dbe2c844f2bdf440819cb060149ad6
Ariane <ariane-rm@hotmail.com>
2023-01-31 22:57:13 -0300

    first commit

---
+++ modules/network/aws_default_route_table.tf
@@ -0,0 +1,15 @@
+resource "aws_default_route_table" "route_table_default" {
+  default_route_table_id = aws_vpc.my_vpc.default_route_table_id #configuração da tabela de roteamento
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(  #uni duas informações
+    local.common_tags,
+    {
+      Name = format("%s_route_table", var.environment)
+    },
+  )
+}
